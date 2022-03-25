4e8504a5484086987a157544c77fc0c3ca35c120
PierreFeo <feokristov.petr@gmail.com>
2022-03-26 01:55:50 +1000

    add

---
+++ src/main/kotlin/ru/netology/main.kt
@@ -0,0 +1,36 @@
+package ru.netology
+
+const val MASTERCARD = "MASTERCARD"
+const val MAESTRO = "MAESTRO"
+const val VISA = "VISA"
+const val MIR = "MIR"
+const val VK = "VK"
+
+fun main() {
+    val transfer = 250000
+    val sumMonth = 324000
+    val paymentType = "VISA"
+    val result = transferMoney(transfer, sumMonth, paymentType)
+    println("Комиссия за перевод: ${result / 100} рублей ${result % 100} копеек.")
+}
+
+fun transferMoney(transfer: Int, sumMonth: Int = 0, paymentType: String = "VK"): Int {
+    val maxSumMonthMasterCard = 75000 * 100
+    val level = when (paymentType) {
+        MASTERCARD, MAESTRO -> if (transfer + sumMonth < maxSumMonthMasterCard) 0 else (transfer / 100 * 0.6 + 2000)
+        VISA, MIR -> if (transfer / 100 * 0.75 < 3500) 3500 else transfer / 100 * 0.75
+        VK -> 0
+        else -> error("Карта не поддерживается")
+    }
+    return level.toInt()
+
+}
+
+
+
+
+
+
+
+
+
