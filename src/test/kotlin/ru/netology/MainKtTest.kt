55941c14a7ff99c99242c4e581d31036b1619136
PierreFeo <feokristov.petr@gmail.com>
2022-03-27 22:18:10 +1000

    manual bug

--- src/test/kotlin/ru/netology/MainKtTest.kt
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -76,7 +76,7 @@ class MainKtTest {
             maxSumMonthMasterCard = maxSumMonthMasterCard
         )
         //assert
-        assertEquals(3500, result)
+        assertEquals(3400, result)
     }
     @Test
     fun transferMoney_commissionVkPay() {
