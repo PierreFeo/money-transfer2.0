952113192797d3a1840721f1ea9edad57a9ad1bd
PierreFeo <feokristov.petr@gmail.com>
2022-03-27 19:55:21 +1000

    add test

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -9,13 +9,14 @@ const val VK = "VK"
 fun main() {
     val transfer = 250000
     val sumMonth = 324000
-    val paymentType = "VISA"
-    val result = transferMoney(transfer, sumMonth, paymentType)
+    val paymentType = "MASTERCARD"
+    val maxSumMonthMasterCard = 75000 * 100
+    val result = transferMoney(transfer, sumMonth, paymentType, maxSumMonthMasterCard)
     println("Комиссия за перевод: ${result / 100} рублей ${result % 100} копеек.")
 }
 
-fun transferMoney(transfer: Int, sumMonth: Int = 0, paymentType: String = "VK"): Int {
-    val maxSumMonthMasterCard = 75000 * 100
+fun transferMoney(transfer: Int, sumMonth: Int = 0, paymentType: String = "VK", maxSumMonthMasterCard: Int): Int {
+
     val level = when (paymentType) {
         MASTERCARD, MAESTRO -> if (transfer + sumMonth < maxSumMonthMasterCard) 0 else (transfer / 100 * 0.6 + 2000)
         VISA, MIR -> if (transfer / 100 * 0.75 < 3500) 3500 else transfer / 100 * 0.75
